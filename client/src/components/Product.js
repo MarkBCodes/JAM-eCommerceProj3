5d297ab1fbde6b262e723654e462ff7f61153467
Julissa Giron <julissa.giron@gmail.com>
2019-10-03 20:17:29 -0400

    latest front-end files components, css, app,js

---
+++ client/src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Product extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is the actual product page</h3>
+      </div>
+    );
+  }
+}
