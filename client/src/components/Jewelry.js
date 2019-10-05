eaefac1aacc892d394a354ee14d9aa90eb88e196
Julissa Giron <julissa.giron@gmail.com>
2019-10-05 08:49:24 -0400

    updated component folders, created context_api.js to start pulling products and syntax for first product page apparel, got nav to link correctly to components

---
+++ client/src/components/Jewelry.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Jewelry extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is where the Jewelry Products will be listed</h3>
+      </div>
+    );
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { InventoryProvider } from "./Context_api";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <InventoryProvider>
+    <Router>
+      <App />
+    </Router>
+  </InventoryProvider>,
   document.getElementById("root")
 );
 
