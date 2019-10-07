65209ab0521fdc3f0e5508675ea81a0970ed07ba
alleykatwill <48265377+alleykatwill@users.noreply.github.com>
2019-10-07 19:51:53 -0400

    empty cart

--- client/src/components/Cart/cartProducts.js
+++ client/src/components/Cart/cartProducts.js
@@ -1,3 +1,6 @@
+// cart buttons are currently set to black. They can be changed to match logo
+// values can be changed to match DB. Need to Make sure img are pulled into cart currently.
+
 import React, { Component } from "react";
 export default class CartItem extends Component {
   render() {
@@ -58,3 +61,16 @@ export default class CartItem extends Component {
     );
   }
 }
+
+// Empy cart
+// export default function EmptyCart() {
+//     return (
+//       <div className="container mt-5">
+//         <div className="row">
+//           <div className="col-10 mx-auto text-center text-title text-capitalize">
+//             <h1>your cart is currently empty</h1>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
