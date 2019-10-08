aaa81ca8ae9bea3223115e12f0b8fef7705ec3b7
alleykatwill <48265377+alleykatwill@users.noreply.github.com>
2019-10-08 17:02:13 -0400

    changes to cart

--- client/src/components/Cart/cartProducts.js
+++ client/src/components/Cart/cartProducts.js
@@ -61,16 +61,3 @@ export default class CartItem extends Component {
     );
   }
 }
-
-// Empy cart
-// export default function EmptyCart() {
-//     return (
-//       <div className="container mt-5">
-//         <div className="row">
-//           <div className="col-10 mx-auto text-center text-title text-capitalize">
-//             <h1>your cart is currently empty</h1>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
