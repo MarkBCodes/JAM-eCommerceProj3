483ea1cb07c3c27df9bc770159f1466b77298812
alleykatwill <48265377+alleykatwill@users.noreply.github.com>
2019-10-10 17:27:38 -0400

    cart updates

--- client/src/Context_api.js
+++ client/src/Context_api.js
@@ -8,16 +8,8 @@ const InventoryContext = React.createContext();
 class InventoryProvider extends Component {
   state = {
     products: [],
-<<<<<<< HEAD
     productItem: productItem,
     cart: []
-=======
-    detailProduct: detailProduct,
-    cart: [],
-    cartSubtotal: 0,
-    cartTax: 0,
-    cartTotal: 0
->>>>>>> e4ca3de85c86150492efc5f04a172e81d6181b14
   };
   componentDidMount() {
     this.setProducts();
